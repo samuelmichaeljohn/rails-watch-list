1b6441100c9fcd12807e100eb45f3790191905a6
samuel smith <samuel-smith@hotmail.com>
2023-08-05 12:10:50 +0100

    added list scaffold

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, :overview, :rating, presence: true
+  # has_many :reviews, dependent: :destroy
+  validates :title, :overview, uniqueness: true
 end
